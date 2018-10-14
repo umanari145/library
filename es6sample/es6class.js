89e715d1a52902c4904a5f8d76701ce4fefa5e8f
Norio <umanari145@gmail.com>
2018-10-15 02:56:10 +0900

    ディレクトリ変更とクラス定義

---
+++ es6sample/es6class.js
@@ -0,0 +1,28 @@
+class PersonClass{
+  constructor (id, name) {
+    this.id = id
+    this.name = name
+  }
+  printName(){
+    console.log(`${this.name} クラスの中に入れている名前です。`)
+  }
+}
+
+let personClass = new PersonClass(1, "太郎")
+personClass.printName()
+
+
+class StudentClass extends PersonClass{
+  constructor(id , name, age ) {
+    super(id, name)
+    this.age = age
+  }
+
+  sendMessage(){
+    console.log(`${this.name}の年齢は${this.age}です`)
+  }
+
+}
+
+let stundent = new StudentClass(2, '松井', 35)
+stundent.sendMessage()
