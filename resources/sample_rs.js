e2f84f4000d9b1876df38699f6858db9749abdc0
Norio <umanari145@gmail.com>
2019-04-14 17:01:14 +0900

    コミット漏れ

--- resources/sample_rs.js
+++ resources/sample_rs.js
@@ -5,13 +5,3 @@ var $ = require('jquery');
 $(function(){
 	$('#hoge').html('bbbb');
 });
-
-const ClassicEditor = require( '@ckeditor/ckeditor5-build-classic' );
-ClassicEditor
-.create( document.querySelector( '#editor' ) )
-.then( editor => {
-		console.log( editor );
-} )
-.catch( error => {
-		console.log( error );
-} );
