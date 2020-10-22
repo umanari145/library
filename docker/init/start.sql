f16ac26ac615978ff8bb843235a1526327e5f928
umanari145 <umanari145@gmail.com>
2020-10-23 00:15:19 +0900

    郵便番号まで一括でinsertを確認

--- docker/init/start.sql
+++ docker/init/start.sql
@@ -1,13 +1,13 @@
 create table postcode (
-    id int not null auto_increment primary key,
     zip VARCHAR(100) COMMENT '郵便番号',
+    pref_kana VARCHAR(100) COMMENT '都道府県(カナ)',
+    city_kana VARCHAR(100) COMMENT '市区町村(カナ)',
+    town_kana VARCHAR(100) COMMENT '町内(カナ)',
     pref VARCHAR(100) COMMENT '都道府県',
     city VARCHAR(100) COMMENT '市区町村',
-    town VARCHAR(100) COMMENT '町内',
-    pref_kana VARCHAR(100) COMMENT '都道府県',
-    city_kana VARCHAR(100) COMMENT '市区町村',
-    town_kana VARCHAR(100) COMMENT '町内',
+    town VARCHAR(100) COMMENT '町内'
 );
 
 ALTER TABLE postcode ADD INDEX idx_zip(zip);
 
+load data local infile '/docker-entrypoint-initdb.d/KEN_ALL.CSV' INTO table postcode FIELDS terminated by ',' enclosed by '"' escaped by '"'
