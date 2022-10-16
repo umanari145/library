156969c97757f6408c645d879316587825c0a362
umanari145 <umanari145@gmail.com>
2022-10-16 21:55:49 +0900

    clone方法を考慮

--- dynamic_element.php
+++ dynamic_element.php
@@ -1,20 +1,27 @@
 <?php
 
-    $json_file_path = 'dynamic_element.json';
-    $furits = @$_POST['fruits'] ?: '';
-    $names = @$_POST['name'] ?: '';
+$json_file_path = 'dynamic_element.json';
+$furits = @$_POST['fruits'] ?: '';
+$name = @$_POST['name'] ?: '';
 
-    //var_dump($furits);
-    //var_dump($names);
-    $elements = [];
-    if (isset($_POST['sumit'])) {
-
-    } else {
-        // 投稿ボタン押してない時
-        if (file_exists($json_file_path)) {
-            $elements = json_decode(file_get_contents($json_file_path), true);
-        }
+//var_dump($furits);
+//var_dump($names);
+$elements = [];
+if (isset($_POST['submit'])) {
+    // バリデーションで引っかかってきた時
+    $count = count($furits);
+    for ($i = 0; $i < $count; $i++) {
+        $elements[] = [
+            'fruits' => $furits[$i],
+            'name' => $name[$i]
+        ];
     }
+} else {
+    // 投稿ボタン押してない時
+    if (file_exists($json_file_path)) {
+        $elements = json_decode(file_get_contents($json_file_path), true);
+    }
+}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml"
@@ -22,6 +29,7 @@
     <head>
    <title>test</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="./css/bundle.css" />
     <body>
     <!-- jsはhtmlの後に読み込ませないとエラーになるので注意 -->
     <script src="./libs/js/main.js"></script>
@@ -33,7 +41,6 @@
 
             init()
 
-            console.log(line_id);
             $(document).on("click", ".add", function(){
                 add_row()
             })
@@ -42,6 +49,8 @@
             })
 
             function init() {
+                
+                $('.select2').select2();
                 if (line_count > 0) {
                     line_id = line_count
                 } else {
@@ -52,10 +61,19 @@
 
             function add_row() {
                 line_id++;
-                $('#raw_line tr').attr('id', 'line_' + line_id);
-                let tr = $('#raw_line').html();
-                $('#raw_line tr').attr('id', '');
-                $('#inbody').append(tr);
+                // 古いやり方
+                //$('#raw_line tr').attr('id', 'line_' + line_id);
+                //let tr = $('#raw_line').html();
+                //$(tr).find('.select2').select2()
+                //$('#raw_line tr').attr('id', '');
+                //$('#inbody').append(tr);
+
+                // https://qiita.com/betti/items/da622a6e5af7135f3e67
+                $clone = $("#raw_line tr").clone(true);
+                $clone.attr('id', 'line_' + line_id);
+                // 動的要素に対して付与
+                $clone.find("select").select2();
+                $clone.appendTo("#inbody");
             }
 
             function remove_row(ele) {
@@ -76,8 +94,8 @@
             <tbody id="inbody">
                 <?php foreach ($elements as $index => $element) : ?>
                 <tr class="line" id="line_<?php echo ($index + 1); ?>">
-                    <td>
-                        <select name="fruits[]" class="select2">
+                    <td width="100px">
+                        <select name="fruits[]" class="select2" value="" style="width:80px;">
                             <option value="1" <?php echo ($element['fruits'] === '1') ? 'selected' : ''; ?>>りんご</option>
                             <option value="2" <?php echo ($element['fruits'] === '2') ? 'selected' : ''; ?>>かき</option>
                             <option value="3" <?php echo ($element['fruits'] === '3') ? 'selected' : ''; ?>>みかん</option>
@@ -102,8 +120,8 @@
     <table style="display:none" >
         <tbody id="raw_line">
             <tr class="line" id="">
-                <td>
-                    <select name="fruits[]">
+                <td width="100px">
+                    <select name="fruits[]" style="width:80px;">
                         <option value="1">りんご</option>
                         <option value="2">かき</option>
                         <option value="3">みかん</option>
