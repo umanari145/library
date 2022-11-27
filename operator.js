c53f70de20282660ea4685303dfddabe0834145e
umanari145 <umanari145@gmail.com>
2022-11-27 12:39:33 +0900

    演算子について

--- operator.js
+++ operator.js
@@ -18,8 +18,21 @@ const formatterNum = typeof num === 'number' ? num.toLocaleString() : '数値で
 console.log(formatterNum);
 
 // ||の本当の意味。orでも良いがは左側がfalseなら右側を返す
-// 変数のdefaultの意味に近い
+// 変数のdefaultの意味に近い &は左側がtrueなら右側を返す
 
 const number = 100;
 const fee = number || '未設定';
 console.log(fee)
+
+// false判定は以下3つ
+const number2 = null;
+const fee2 = number2 || '未設定';
+console.log(fee2)
+
+const number3 = undefined;
+const fee3 = number3 || '未設定';
+console.log(fee3)
+
+const number4 = '';
+const fee4 = number4 || '未設定';
+console.log(fee4)
