04fa9190dabdbf8aacd476b97c24dbf09779c1cd
umanari145 <umanari145@gmail.com>
2022-11-27 11:30:56 +0900

    三項目演算子

--- operator.js
+++ operator.js
@@ -12,3 +12,14 @@ console.log(var2);
 console.log("エルビス")
 var2 = var1 || 'hoge';
 console.log(var2);
+
+const num = 1300;
+const formatterNum = typeof num === 'number' ? num.toLocaleString() : '数値でお願いします。'
+console.log(formatterNum);
+
+// ||の本当の意味。orでも良いがは左側がfalseなら右側を返す
+// 変数のdefaultの意味に近い
+
+const number = 100;
+const fee = number || '未設定';
+console.log(fee)
